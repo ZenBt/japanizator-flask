36a0d99daff98ff0d251c7f24acee61e23eeb0a2
ZenBt <zenbee111@gmail.com>
2022-03-27 17:46:31 +0300

    added supprt of customization history

--- view.py
+++ view.py
@@ -1,15 +1,28 @@
-from flask import render_template, url_for, request, jsonify
+from flask import render_template, url_for, request, jsonify, session, redirect
 from app import app
 from external_scripts.jap import stylish
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        history = session.get('history')
+        return render_template('index.html', history=history)
     elif request.method == 'POST':
         raw = request.form.get('raw_text')
         output = stylish(raw)
-        return render_template('index.html', output=output)
+        if session.get('history') is not None:
+            history = session['history']
+            history.append((raw, output))
+            session['history'] = history
+        else:
+            session['history'] = [(raw, output)]
+            history = session['history']
+        return render_template('index.html', output=output, history=history)
+
+@app.route('/clear')
+def clear_session():
+    del session['history']
+    return redirect(url_for('index'))
 
 
 @app.errorhandler(404)
@@ -28,3 +41,4 @@ def get_styled(sentence):
         'styled': output
     }
     return jsonify(response)
+    
